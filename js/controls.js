04049a6439366fbc5ed49ce746cf36dcdbc86177
Peter Briers <peter.briers@gmail.com>
2018-09-21 12:57:30 +0200

    Set all controls to an object

--- js/controls.js
+++ js/controls.js
@@ -1,17 +1,28 @@
 import {ControlCar} from './control-car.js';
 import {ControlSave} from './control-save.js';
 
-export function initControls() {
-  new ControlCar('app-control-main-brand', 'main.brand');
-  new ControlCar('app-control-main-model', 'main.model', 'checked');
-  new ControlCar('app-control-cabine-color', 'cabine.color');
-  new ControlCar('app-control-cabine-offset', 'cabine.offset');
-  new ControlCar('app-control-cabine-size', 'cabine.size');
-  new ControlCar('app-control-body-color', 'body.color');
-  new ControlCar('app-control-body-offset', 'body.offset');
-  new ControlCar('app-control-body-size', 'body.size');
-  new ControlCar('app-control-wheels-color', 'wheels.color');
-  new ControlCar('app-control-wheels-size', 'wheels.size');
-  new ControlSave();
-}
+let controls = {};
 
+export function initControls() {
+  controls = {
+    main: {
+      brand: new ControlCar('app-control-main-brand', 'main.brand'),
+      model: new ControlCar('app-control-main-model', 'main.model', 'checked'),
+    },
+    cabine: {
+      color: new ControlCar('app-control-cabine-color', 'cabine.color'),
+      offset: new ControlCar('app-control-cabine-offset', 'cabine.offset'),
+      size: new ControlCar('app-control-cabine-size', 'cabine.size'),
+    },
+    body: {
+      color: new ControlCar('app-control-body-color', 'body.color'),
+      offset: new ControlCar('app-control-body-offset', 'body.offset'),
+      size: new ControlCar('app-control-body-size', 'body.size'),
+    },
+    wheels: {
+      color: new ControlCar('app-control-wheels-color', 'wheels.color'),
+      size: new ControlCar('app-control-wheels-size', 'wheels.size'),
+    },
+    save: new ControlSave()
+  }
+}
