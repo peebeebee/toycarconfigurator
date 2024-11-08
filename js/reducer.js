87e4f0a7c1ad1d2aa996e7fa006e0a09b04c7a91
Peter Briers <peter.briers@gmail.com>
2024-11-08 16:20:58 +0100

    Fix state bugs, and improve scene render

--- js/reducer.js
+++ js/reducer.js
@@ -1,22 +1,26 @@
-import merge from 'deepmerge';
+import merge from "deepmerge";
 
 export const reducer = (state = {}, action = {}) => {
   const createNewState = (changes) => Object.freeze(merge(state, changes));
+  let newState;
   switch (action.type) {
-  case 'LOAD':
-    return createNewState({
-      car: action.payload.car,
-      loading: false
-    });
-  case 'SET_CAR':
-    return createNewState({
-      car: action.payload
-    });
-  case 'SET_SCENE':
-    return createNewState({
-      scene: action.payload
-    });
-  default:
-    return state;
+    case "LOAD":
+      newState = createNewState({
+        car: action.payload.car,
+        loading: false,
+      });
+      return newState;
+    case "SET_CAR":
+      newState = createNewState({
+        car: action.payload,
+      });
+      return newState;
+    case "SET_SCENE":
+      newState = createNewState({
+        scene: action.payload,
+      });
+      return newState;
+    default:
+      return state;
   }
-}
+};
--- js/store.js
+++ js/store.js
@@ -1,43 +1,43 @@
 const baseState = {
   car: {
     main: {
-      brand: 'BMW',
-      model: 'racecar'
+      brand: "BMW",
+      model: "racecar",
     },
     cabine: {
-      color: '#d4e0e6',
+      color: "#d4e0e6",
       offset: 0,
       size: 1.3,
     },
     body: {
-      color: '#e65757',
+      color: "#e65757",
       offset: 23,
       size: 1.05,
     },
     wheels: {
-      color: '#ffef00',
+      color: "#ffef00",
       size: 1.15,
     },
   },
   scene: {
     rotateX: 0,
     rotateZ: 0,
-    zoom: 1
-  }
-}
+    zoom: 1,
+  },
+};
 
 let store, listeners;
 
 export function initStore(reducer, state = baseState) {
-  let newState = Object.freeze(reducer(baseState)); // baseState should be newState1
+  let newState = Object.freeze(reducer(state));
   listeners = [];
   store = {};
   store.dispatch = (action) => {
-    newState = Object.freeze(reducer(state, action));
-    listeners.forEach(fn => fn());
-  }
+    newState = Object.freeze(reducer(newState, action));
+    listeners.forEach((fn) => fn());
+  };
   store.subscribe = (fn) => listeners.push(() => fn(newState));
   store.getState = () => newState;
 }
 
-export {store};
+export { store };
